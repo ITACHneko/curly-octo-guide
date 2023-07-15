895eb4ec9a9d081cdb1d599e16a08d4b2ec27072
ITACHneko <139033188+ITACHneko@users.noreply.github.com>
2023-07-15 21:57:36 +0330

    Update homework.py

--- homework.py
+++ homework.py
@@ -1,11 +1,11 @@
-num_students = int(input("Enter the number of students in the class: "))
+a = int(input("tedad danesh amozan ra benevisid: "))
 
 grades = []
-for i in range(num_students):
+for i in range(a):
     grade = float(input("Enter the grade for student {}: ".format(i + 1)))
     grades.append(grade)
 
-average_grade = sum(grades) / num_students
+average_grade = sum(grades) / a
 min_grade = min(grades)
 
 print("Average grade:", average_grade)
