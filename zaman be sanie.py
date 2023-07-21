1f07ec4c314359b16849d14ca9a7261653cd8893
ITACHneko <139033188+ITACHneko@users.noreply.github.com>
2023-07-21 22:06:57 +0330

    Add files via upload

---
+++ zaman be sanie.py
@@ -0,0 +1,7 @@
+sat = int(input("sat ra vard konid: "))
+dagige = int(input("dagige ra vared konid: "))
+sanie = int(input("sanie ra vared konid : "))
+
+zaman  = (sat * 3600) + (dagige * 60) + sanie
+
+print("tabdil shode be sanie :", zaman )
