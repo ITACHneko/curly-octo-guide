0c6a4b643692da35722a174ea29b889f3be4d220
ITACHneko <139033188+ITACHneko@users.noreply.github.com>
2023-07-21 22:07:13 +0330

    Add files via upload

---
+++ tas.py
@@ -0,0 +1,11 @@
+import random
+
+while True:
+    t = random.randint(1, 6)
+    print("The dice rolled:", t)
+    if t == 6:
+        print("tabrik 6 avardid .dobare partab konid")
+    else:
+        break
+
+print("adad nahaie partab shode:", t)
