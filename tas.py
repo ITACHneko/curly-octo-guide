5b1efe1477c8253424d383a778afb4874a7a1458
ITACHneko <139033188+ITACHneko@users.noreply.github.com>
2023-07-21 22:47:12 +0330

    Update tas.py

--- tas.py
+++ tas.py
@@ -2,7 +2,7 @@ import random
 
 while True:
     t = random.randint(1, 6)
-    print("The dice rolled:", t)
+    print("adad amade:", t)
     if t == 6:
         print("tabrik 6 avardid .dobare partab konid")
     else:
